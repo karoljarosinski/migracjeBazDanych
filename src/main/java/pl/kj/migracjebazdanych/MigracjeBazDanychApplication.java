7f95155e1b1a20c578bc4bd9310733b685badc20
karol <karol.jarosinski91@gmail.com>
2021-05-14 05:10:50 +0200

    empty

---
+++ src/main/java/pl/kj/migracjebazdanych/MigracjeBazDanychApplication.java
@@ -0,0 +1,13 @@
+package pl.kj.migracjebazdanych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracjeBazDanychApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigracjeBazDanychApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kj/migracjebazdanych/MigracjeBazDanychApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kj.migracjebazdanych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigracjeBazDanychApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
