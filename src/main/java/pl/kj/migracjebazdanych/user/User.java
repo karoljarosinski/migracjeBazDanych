b89a229b3d7e83ab344a4dd2f1958953efa0f89d
karol <karol.jarosinski91@gmail.com>
2021-05-14 09:08:32 +0200

    part 1

---
+++ src/main/java/pl/kj/migracjebazdanych/user/User.java
@@ -0,0 +1,63 @@
+package pl.kj.migracjebazdanych.user;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+
+    public User(){}
+
+    public User(String firstName, String lastName, LocalDate birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO USER
+(first_name, last_name, birthday)
+VALUES
+('Jan', 'Kowalski', '1990-06-23'),
+('Damian', 'Trabacki', '1989-03-23'),
+('Irena', 'Nowacka', '1975-06-15');
